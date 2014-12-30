5fa7b3fac5942fc466a48339763f4d5d0fd4cbec
patrick brisbin <pbrisbin@gmail.com>
2014-12-30 18:35:57 -0500

    Remove img compiler from main.hs

--- main.hs
+++ main.hs
@@ -18,7 +18,7 @@ main = hakyll $ do
     tags <- buildTags "posts/*" $ fromCapture "tags/*/index.html"
     navigation <- buildNavigation "posts/*"
 
-    match ("favicon.ico" .||. "css/**" .||. "img/**") $ do
+    match ("favicon.ico" .||. "css/**") $ do
         route idRoute
         compile copyFileCompiler
 
